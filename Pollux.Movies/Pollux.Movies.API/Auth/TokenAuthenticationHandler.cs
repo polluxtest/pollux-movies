b61198fc48340d63fd3d4fae3c259abbfe8ac9c9
gitName <a@b.c>
2022-05-30 09:14:44 -0500

    added compilation stuff

--- Pollux.Movies/Pollux.Movies.API/Auth/TokenAuthenticationHandler.cs
+++ Pollux.Movies/Pollux.Movies.API/Auth/TokenAuthenticationHandler.cs
@@ -26,12 +26,22 @@ namespace Pollux.Movies.Auth
     {
         private readonly IServiceProvider serviceProvider;
         private readonly IConfiguration configuration;
+        private readonly ILogger<ApplicationLogger> logger;
 
-        public TokenAuthenticationHandler(IOptionsMonitor<TokenAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IServiceProvider serviceProvider, IConfiguration configuration)
-       : base(options, logger, encoder, clock)
+        public TokenAuthenticationHandler(
+            IOptionsMonitor<TokenAuthenticationOptions> options,
+            ILogger<ApplicationLogger> logger,
+            ILoggerFactory loggerFactory,
+            UrlEncoder encoder,
+            ISystemClock clock,
+            IServiceProvider serviceProvider,
+            IConfiguration configuration)
+       : base(options, loggerFactory, encoder, clock)
         {
+            this.logger = logger;
             this.serviceProvider = serviceProvider;
             this.configuration = configuration;
+            this.logger = logger;
         }
 
         /// <summary>
@@ -43,32 +53,41 @@ namespace Pollux.Movies.Auth
             try
             {
                 this.Request.Headers.TryGetValue("Authorization", out var token);
+                this.logger.LogInformation($"token {token}");
                 var claims = new[] { new Claim("token", token) };
                 var identity = new ClaimsIdentity(claims, nameof(TokenAuthenticationHandler));
                 var ticket = new AuthenticationTicket(new ClaimsPrincipal(identity), this.Scheme.Name);
 
                 if (!string.IsNullOrEmpty(token))
                 {
+                    this.logger.LogInformation("token is not null and token decoded is the same ok.");
                     if (token.ToString().Contains("Bearer", StringComparison.OrdinalIgnoreCase))
                     {
                         token = token.ToString().Remove(0, 7);
                         bool isValid = this.ValidateToken(token);
                         if (isValid)
                         {
+                            this.logger.LogInformation("Token is valid");
                             return Task.FromResult(AuthenticateResult.Success(ticket));
                         }
                         else
                         {
+                            this.logger.LogInformation("Token is invalid");
                             return Task.FromResult(AuthenticateResult.Fail("Not Authenticated"));
                         }
                     }
                 }
 
+                this.logger.LogInformation("Authorization failed 401");
                 this.Response.StatusCode = StatusCodes.Status401Unauthorized;
                 return Task.FromResult(AuthenticateResult.Fail("Not Authenticated"));
             }
             catch (Exception ex)
             {
+                this.logger.LogInformation("Authorization exception");
+                this.logger.LogInformation(ex.Message);
+                this.logger.LogInformation(ex.StackTrace);
+                this.logger.LogInformation(ex?.InnerException.Message);
                 this.Response.StatusCode = StatusCodes.Status401Unauthorized;
                 return Task.FromResult(AuthenticateResult.Fail("Not Authenticated"));
             }
@@ -83,29 +102,42 @@ namespace Pollux.Movies.Auth
             try
             {
                 var tokenIssuer = this.configuration.GetSection("AppSettings")["TokenIssuer"];
+                this.logger.LogInformation($"token issuer app settings {tokenIssuer}");
                 var signingKeyId = this.configuration.GetSection("AppSettings")["SigningKeyId"];
                 var tokenHandler = new JwtSecurityTokenHandler();
                 var securityToken = tokenHandler.ReadJwtToken(token);
+                this.logger.LogInformation($"token decoded jwt {tokenIssuer}");
                 var securityTokenSigningkeyId = securityToken.Header["kid"];
                 if (securityToken.Issuer.Equals(tokenIssuer) && signingKeyId.Equals(securityTokenSigningkeyId))
                 {
                     var claims = securityToken.Claims.ToList();
+                    this.logger.LogInformation($"claims  {claims}");
+                    this.logger.LogInformation("token issuer and token decoded is the same ok.");
                     var expiration = claims[1].Value;
+
+                    this.logger.LogInformation($"expiration  {expiration}");
+
                     var expirationDateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(expiration));
                     if (DateTime.UtcNow > expirationDateTime)
                     {
+                        this.logger.LogInformation($"expired token");
                         return false;
                     }
                 }
                 else
                 {
+                    this.logger.LogInformation("token issuer and token decoded is NOT the same failed.");
                     return false;
                 }
 
                 return true;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                this.logger.LogError($"error exception  {ex.Message}");
+                this.logger.LogError($"error exception  {ex.StackTrace}");
+                this.logger.LogError($"error exception  {ex.InnerException}");
+
                 return false;
             }
         }
